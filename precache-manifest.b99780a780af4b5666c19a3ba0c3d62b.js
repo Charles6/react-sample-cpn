154fd8ce3c18589901c559681c2e36f740ea14e5
Charles Nguyen <cpnguy@gmail.com>
2019-11-26 12:32:32 -0800

    Updates

---
+++ precache-manifest.b99780a780af4b5666c19a3ba0c3d62b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8597c365aa800cbece90255b503b6fd6",
+    "url": "/react-sample-cpn/index.html"
+  },
+  {
+    "revision": "fb78a99306eca9419743",
+    "url": "/react-sample-cpn/static/css/main.4337bc4c.chunk.css"
+  },
+  {
+    "revision": "3de53e771742a80e7851",
+    "url": "/react-sample-cpn/static/js/2.e8da73e6.chunk.js"
+  },
+  {
+    "revision": "fb78a99306eca9419743",
+    "url": "/react-sample-cpn/static/js/main.33676301.chunk.js"
+  },
+  {
+    "revision": "606e42872abbed5833ad",
+    "url": "/react-sample-cpn/static/js/runtime-main.3c654dba.js"
+  },
+  {
+    "revision": "6815ca0381b19bd135838c906584af04",
+    "url": "/react-sample-cpn/static/media/coffee.6815ca03.gif"
+  }
+]);
