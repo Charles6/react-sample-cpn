df8cfdf6ea51d4295be784a4f74f3bf14625dfcc
Charles Nguyen <cpnguy@gmail.com>
2019-12-30 09:02:38 -0800

    Updates

--- precache-manifest.56b1306cb0af1b235446a4ff887f24dc.js
+++ precache-manifest.56b1306cb0af1b235446a4ff887f24dc.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "86b8a396364ed7cf2d8103f41cca2fab",
+    "revision": "b576c5028c15c0fd08b6057d03e47e9b",
     "url": "/react-sample-cpn/index.html"
   },
   {
-    "revision": "d08a46987e9c7f07afb5",
-    "url": "/react-sample-cpn/static/css/main.dc3728a4.chunk.css"
+    "revision": "5cfb6e3bbdc15b4c7815",
+    "url": "/react-sample-cpn/static/css/main.68e304b8.chunk.css"
   },
   {
     "revision": "62c05af8484c1e7aaa60",
     "url": "/react-sample-cpn/static/js/2.fd70d03c.chunk.js"
   },
   {
-    "revision": "d08a46987e9c7f07afb5",
-    "url": "/react-sample-cpn/static/js/main.59851247.chunk.js"
+    "revision": "5cfb6e3bbdc15b4c7815",
+    "url": "/react-sample-cpn/static/js/main.835d948a.chunk.js"
   },
   {
     "revision": "606e42872abbed5833ad",
